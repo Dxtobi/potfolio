661051de561fd42878171822d0c3b6b13b4da7a6
dxtobi <akanbijosephtobi@gmail>
2023-08-15 07:35:02 -0700

    udated head

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,20 +11,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>JAO</title>
 	<meta name="title" content="Joseph Akanbi Oluwatobi">
-	<meta property="og:description" content="Full Stack Node Developer - TOBi-JAO">
+	<meta property="og:description" content="Experienced software developer and data analyst with background in web development,
+					software proficiency,">
 	
 	<meta name="author" content="@programmer_dex " />
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website">
 	
 	<meta property="og:title" content="Joseph Akanbi Oluwatobi">
-	<meta property="og:description" content="Full Stack Node Developer - TOBi-JAO">
+	<meta property="og:description" content="Experienced software developer and data analyst with background in web development,
+					software proficiency,">
 	<meta property="og:image" content="https://i.pinimg.com/564x/75/46/30/7546306dbc194f5b555c3ff6cf44a91b.jpg">
 	<meta property="og:url" content="https://jaodev.vercel.app/" />
 	<!-- Twitter -->
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:title" content="">
-	<meta name="twitter:description" content="Full Stack Node Developer - TOBi-JAO">
+	<meta name="twitter:description" content="Experienced software developer and data analyst with background in web development,
+					software proficiency,">
 	<meta name="twitter:image" content="https://i.pinimg.com/564x/75/46/30/7546306dbc194f5b555c3ff6cf44a91b.jpg"/>
 </svelte:head>
 
